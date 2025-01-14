965fa42ac2c177a705e777b25efe4b2ab03827c5
jesse <nitchsx@gmail.com>
2025-01-14 13:26:46 -0300

    Feat: first commit

---
+++ linguagem-dart/first_dart_code/bin/first_dart_code.dart
@@ -0,0 +1,5 @@
+import 'package:first_dart_code/first_dart_code.dart' as first_dart_code;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first_dart_code.calculate()}!');
+}
---
+++ linguagem-dart/first_dart_code/test/first_dart_code_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_dart_code/first_dart_code.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
