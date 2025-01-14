ab71b0d1371f92bc6d2df16df746456d19a467f4
jesse <nitchsx@gmail.com>
2025-01-14 14:12:23 -0300

    feat: class of types

--- linguagem-dart/first_dart_code/bin/first_dart_code.dart
+++ linguagem-dart/first_dart_code/bin/first_dart_code.dart
@@ -1,5 +1,13 @@
 import 'package:first_dart_code/first_dart_code.dart' as first_dart_code;
 
 void main(List<String> arguments) {
+  String name = "jesse";
+  int age = 20;
+  double age1 = 20.3;
+
+  List<String> aa = ["eu", "nós", "vós"]; //uma lista de strings
+
+  print('Olá $name a have $age years old or $age1');
+  print(aa[0]);
   print('Hello world: ${first_dart_code.calculate(5, 1)}!');
 }
